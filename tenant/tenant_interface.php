36888e694f76f993c79c8864983e0a14563f32ce
emamhossaineasin <120816384+emamhossaineasin@users.noreply.github.com>
2023-02-16 01:03:59 +0600

    15-02-2023
    tenant version-1
    Author: Emam Hossain

---
+++ tenant/tenant_interface.php
@@ -0,0 +1,49 @@
+<?php
+    include_once('../connect.php'); 
+    if (isset($_GET['id'])) {
+        $userid = $_GET['id']; 
+        $sql = "SELECT * from apartment where concat(holdingNumber, ApartmentID, '@ras.com')='$userid' limit 1";
+        $result = mysqli_query($con, $sql); 
+        $row = mysqli_fetch_assoc($result); 
+        $sql1 = "SELECT * from tenant where concat(holdingNumber, ApartmentID, '@ras.com')='$userid' limit 1";
+        $result1 = mysqli_query($con, $sql1);
+        $row1 = mysqli_fetch_assoc($result1); 
+    }
+?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>TENANT VIEW</title>
+    </head>
+    <body>
+        <div class="container">
+            <table class="table">
+                <h1> Info about your apartment.</h1>
+                <thead>
+                    <tr>
+                    <th scope="col">Holding Number of Building</th>
+                    <th scope="col">Apartment ID</th>
+                    <th scope="col">Date You Arrived.</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr> 
+                        <td><?php $temp = $row['holdingNumber']; echo $temp; ?></td>
+                        
+                        <td><?php $temp = $row['ApartmentID']; echo $temp; ?></td>
+                        
+                        <td><?php $temp = $row1['registerddate']; echo $temp; ?></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </body>
+</html>
