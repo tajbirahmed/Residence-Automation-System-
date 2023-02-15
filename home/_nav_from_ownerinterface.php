2e5ea2dcd3e58d135f07c2987e9b5715c307857e
Shahriad50 <82698995+Shahriad50@users.noreply.github.com>
2023-02-16 01:06:54 +0600

    home_page version-1

---
+++ home/_nav_from_ownerinterface.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="index.php">Home</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+    <li class="nav-item active">
+        <a class="nav-link" href="Building/Apartments/index.php">Apartment View <span class="sr-only">(current)</span></a>
+      </li>
+          <?php  
+            if (!isset($_SESSION['email']) && !isset($_SESSION['type'])) {
+              
+              echo '<li class="nav-item active">
+                  <a class="nav-link" href="ProfileSystem/login.php">Login <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="ProfileSystem/signup.php">Sign Up</a>
+                        </li>';
+              }
+            ?>
+    <?php 
+        if (isset($_SESSION['email']) && isset($_SESSION['type'])){
+          echo '<li class="nav-item">
+            <a class="nav-link" href="ProfileSystem/logout.php">Logout</a>
+          </li>';
+        }
+          ?>
+      
+    </ul>
+  </div>
+</nav>
