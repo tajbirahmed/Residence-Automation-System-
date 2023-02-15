654e52103803d332811dca3b75c910f6909299d7
tajbirahmed <103434615+tajbirahmed@users.noreply.github.com>
2023-02-16 01:07:30 +0600

    15 feb 23
    version 2
    Author - Tajbir

--- owner/showBuildinginfo.php
+++ owner/showBuildinginfo.php
@@ -14,9 +14,10 @@
 
 <?php
     include_once('../connect.php');
+    
     if (isset($_GET['showHolding'])) {
         $hld = $_GET['showHolding']; 
-        $sql = "select * from building where holdingNumber=$hld";
+        $sql = "select * from `building` where holdingNumber=$hld";
         $result = mysqli_query($con, $sql); 
        
         while ($row = mysqli_fetch_assoc($result)) {
@@ -35,45 +36,68 @@
         ?>
          <div class="container my-5">
               <table class="table">
+              
                     <thead>
                         <tr>  
-                            <th colspan="4" style="text-align:center">Apartments</th>
+                            <th colspan="7" style="text-align:center; font-size: 30px">Apartments</th>
                         </tr>
                         <tr>
-                            <th scope="col">Apartment ID</th>
-                            <th scope="col">Size </th>
-                            <th scope="col">Rent per Month</th>
-                            <th scope="col">Availability</th>
-                            <th scope="col">Rent Collection Date</th>
-                            <th scope="col">Rent Status</th>
-                            <th scope="col">Action</th>
+                            <th scope="col" style="text-align: center">Apartment ID</th>
+                            <th scope="col" style="text-align: center">Size </th>
+                            <th scope="col" style="text-align: center">Rent per Month</th>
+                            <th scope="col" style="text-align: center">Status</th>
+                            <th scope="col" style="text-align: center">Rent Collection Date</th>
+                            <th scope="col" style="text-align: center">Rent Status</th>
+                            <th scope="col" style="text-align: center">Action</th>
+                            
                         </tr>
                     </thead>
                      <tbody>
         <?php
-            $sql = "select * from apartment where holdingNumber=$hld";
-            $result = mysqli_query($con, $sql); 
-       
-            while ($row = mysqli_fetch_assoc($result)) {
-                $aid = $row['ApartmentID']; 
-                $rpdn = $row['rentpermonth']; 
-                $size = $row['size']; 
-                $avl=$row['availability'] ? "YES" : "NO";
-                
-                echo '  <tr>
-                <td>'.$aid.'</td>
-                <td>'.$size.'</td>
-                <td>'.$rpdn.'</td>
-                <td>'.$avl.'</td>
-                </tr>';
-                
+            include_once('../connect.php');
+            if (isset($hld)) {
+                $sql = "select * from apartment where holdingNumber=$hld";
+                $result = mysqli_query($con, $sql); 
+        
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $aid = $row['ApartmentID']; 
+                    $rpdn = $row['rentpermonth']; 
+                    $size = $row['size']; 
+                    $avl=$row['availability'] ? "Empty" : "Rented";
+                    if ($avl == "Empty") 
+                        $col = "red"; 
+                    else 
+                        $col = "green";
+                    
+                    echo '  <tr>
+                    <td style="text-align: center;font-weight: bold;">'.$aid.'</td>
+                    <td style="text-align: center;">'.$size.'</td>
+                    <td style="text-align: center; ">'.$rpdn.'</td>
+                    <td style="text-align: center; color: '.$col.'; font-weight: bold; font-size: 19px">'.$avl.'</td>
+                    <td style="text-align: center;"> </td>
+                    <td style="text-align: center;"> </td>';
+                    
+                    echo '<td style="text-align: center;">'; 
+                    if (!$row['availability'])
+                        echo '<a href="../Building/tenant_info.php?aid='.$aid.'&hld='.$hld.'"><button class="btn btn-success">Details</button></a>';
+                    if ($row['availability'])
+                        echo '<a href="../Building/Apartments/tenant_update.php?aid='.$aid.'&hld='.$hld.'"><button class="btn btn-primary">Update</button></a>';
+                    echo '<button class="btn btn-danger">Delete Apartment</button>
+                    </td>
+                    </tr>';
+                    
+                    }
                 }
         ?>
                 </tbody>
             </table>
         </div>
-
-
-    
+            <div class="container">
+                <form method="post" action="../Building/Apartments/addapartment.php?id=<?php echo $hld ?>">
+                    <h3>Add Apartment to Building</h3>
+                    <button class="btn btn-success">Add Apartment</button>
+                    
+                </form>
+            </div>
         </body>
 </html>
