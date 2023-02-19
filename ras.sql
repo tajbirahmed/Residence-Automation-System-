fa71ce7558331ec57328438354f60c15ec66fb51
tajbirahmed <103434615+tajbirahmed@users.noreply.github.com>
2023-02-19 18:02:33 +0600

    19-02-23
    Secod Stable Version
    Author: Tajbir

--- ras.sql
+++ ras.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 15, 2023 at 07:34 PM
+-- Generation Time: Feb 19, 2023 at 01:01 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -41,57 +41,20 @@ CREATE TABLE `apartment` (
 --
 
 INSERT INTO `apartment` (`ApartmentID`, `holdingNumber`, `rentpermonth`, `size`, `availability`, `BHK`) VALUES
-('', 0, 0, 0, 1, 0),
-('1A', 0, 22000, 2000, 1, 4),
-('1A', 12332, 22000, 2000, 1, 2),
-('1A', 12345, 25000, 2200, 0, 3),
-('1A', 12346, 25000, 2200, 0, 1),
-('1A', 12347, 25000, 2200, 0, 3),
-('1A', 12348, 25000, 2200, 0, 2),
-('1B', 12345, 25000, 2200, 0, 2),
-('1B', 12346, 25000, 2200, 0, 2),
-('1B', 12347, 25000, 2200, 0, 1),
-('1B', 12348, 25000, 2200, 0, 1),
-('1C', 12345, 25000, 2200, 1, 4),
-('1C', 12346, 25000, 2200, 1, 5),
-('1C', 12347, 25000, 2200, 1, 3),
-('1C', 12348, 25000, 2200, 1, 2),
-('1D', 12345, 25000, 2200, 0, 3),
-('1D', 12346, 25000, 2200, 0, 2),
-('1D', 12347, 25000, 2200, 0, 1),
-('1D', 12348, 25000, 2200, 0, 2),
-('2A', 12345, 22000, 2100, 1, 2),
-('2A', 12346, 22000, 2100, 1, 3),
-('2A', 12347, 22000, 2100, 1, 2),
-('2A', 12348, 22000, 2100, 1, 1),
-('2B', 12345, 24000, 2300, 1, 3),
-('2B', 12346, 24000, 2300, 1, 2),
-('2B', 12347, 24000, 2300, 1, 2),
-('2B', 12348, 24000, 2300, 1, 2),
-('2C', 12345, 24000, 2100, 0, 2),
-('2C', 12346, 24000, 2100, 0, 0),
-('2C', 12347, 24000, 2100, 0, 0),
-('2C', 12348, 24000, 2100, 0, 0),
-('2D', 12345, 28000, 2500, 1, 0),
-('2D', 12346, 28000, 2500, 1, 0),
-('2D', 12347, 28000, 2500, 1, 0),
-('2D', 12348, 28000, 2500, 1, 0),
-('3A', 12345, 24000, 2300, 1, 0),
-('3A', 12346, 24000, 2300, 1, 0),
-('3A', 12347, 24000, 2300, 1, 0),
-('3A', 12348, 24000, 2300, 1, 0),
-('3B', 12345, 19000, 2000, 0, 0),
-('3B', 12346, 19000, 2000, 0, 0),
-('3B', 12347, 19000, 2000, 0, 0),
-('3B', 12348, 19000, 2000, 0, 0),
-('3C', 12345, 18000, 2300, 1, 0),
-('3C', 12346, 18000, 2300, 1, 0),
-('3C', 12347, 18000, 2300, 1, 0),
-('3C', 12348, 18000, 2300, 1, 0),
-('3D', 12345, 25000, 2500, 1, 0),
-('3D', 12346, 25000, 2500, 1, 0),
-('3D', 12347, 25000, 2500, 1, 0),
-('3D', 12348, 25000, 2500, 1, 0);
+('12346-1A', 12346, 23000, 2200, 0, 3),
+('12346-1B', 12346, 22000, 2000, 0, 2),
+('12346-1C', 12346, 24000, 2300, 0, 3),
+('12346-1D', 12346, 23000, 2200, 0, 3),
+('12346-2A', 12346, 20000, 2100, 0, 2),
+('12347-1A', 12347, 25000, 2200, 0, 3),
+('12347-1B', 12347, 25000, 2200, 0, 1),
+('12347-1C', 12347, 25000, 2200, 0, 3),
+('12347-1D', 12347, 25000, 2200, 0, 1),
+('12348-1A', 12348, 25000, 2200, 0, 2),
+('12348-1B', 12348, 25000, 2200, 0, 1),
+('12348-1D', 12348, 25000, 2200, 0, 2),
+('12349-1A', 12349, 23000, 2400, 0, 2),
+('12349-1B', 12349, 26000, 2100, 0, 3);
 
 -- --------------------------------------------------------
 
@@ -102,21 +65,23 @@ INSERT INTO `apartment` (`ApartmentID`, `holdingNumber`, `rentpermonth`, `size`,
 CREATE TABLE `building` (
   `holdingNumber` bigint(20) NOT NULL,
   `buildingName` varchar(200) NOT NULL,
-  `image` varchar(150) NOT NULL,
-  `ownerID` bigint(20) NOT NULL
+  `image` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `building`
 --
 
-INSERT INTO `building` (`holdingNumber`, `buildingName`, `image`, `ownerID`) VALUES
-(12332, 'Jurgen Klopp Housing Ltd.', '', 9),
-(12333, 'Salah Palace Ltd.', '', 9),
-(12345, 'ABC Limited', '', 0),
-(12346, 'DEF Limited', '', 0),
-(12347, 'GHI Limited', '', 0),
-(12348, 'JKL Limited', '', 0);
+INSERT INTO `building` (`holdingNumber`, `buildingName`, `image`) VALUES
+(12345, 'Professor Villa', 'building\\12345.jpg'),
+(12346, 'Nilima House', 'building\\12346.jpg'),
+(12347, 'Moniloy Cottage', 'building\\12347.jpg'),
+(12348, 'Riton Flat', 'building\\12348.jpg'),
+(12349, 'Liverpool Enterprise', 'building\\12349.jpg'),
+(12350, 'Manchester City Palace', 'building\\12350.jpg'),
+(12351, 'Salah Palace Ltd.', 'building/12351.jpg'),
+(12352, 'Aston Villa', 'building/12352.jpg'),
+(12353, 'Messi Palace', 'building/12353.jpg');
 
 -- --------------------------------------------------------
 
@@ -138,10 +103,37 @@ CREATE TABLE `location` (
 --
 
 INSERT INTO `location` (`holdingNumber`, `street`, `city`, `area`, `thana`, `houseNo`) VALUES
-(12345, 'Road no. 2', 'Chattogram', 'Sugondha', 'Panchlaish', 25),
-(12346, 'Road no. 5', 'Chattogram', 'Cosmopoliton', 'Panchlaish', 33),
+(12345, 'Road No.2', 'Chattogram', 'Sughandha', 'Panchlaish', 25),
+(12346, 'Road No. 5', 'Chattogram', 'Cosmopoliton', 'Panchlaish', 33),
 (12347, 'Road no. 1', 'Chattogram', 'Chandgaon', 'Chandgaon', 23),
 (12348, 'Road no. 8', 'Chattogram', 'CDA', 'Agrabad', 41);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `own`
+--
+
+CREATE TABLE `own` (
+  `email` varchar(100) NOT NULL,
+  `holdingNumber` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `own`
+--
+
+INSERT INTO `own` (`email`, `holdingNumber`) VALUES
+('alvarez19@gmail.com', 12353),
+('cr7@gmail.com', 12345),
+('cr7@gmail.com', 12347),
+('haaland9@gmail.com', 12350),
+('km7@gmail.com', 12346),
+('martinez23@gmail.com', 12352),
+('messi10@gmail.com', 12345),
+('neymar10@gmail.com', 12348),
+('salah11@gmail.com', 12349),
+('salah11@gmail.com', 12351);
 
 -- --------------------------------------------------------
 
@@ -150,8 +142,6 @@ INSERT INTO `location` (`holdingNumber`, `street`, `city`, `area`, `thana`, `hou
 --
 
 CREATE TABLE `owner` (
-  `ownerID` bigint(20) NOT NULL,
-  `holdingNumber` bigint(20) NOT NULL,
   `first_name` varchar(100) NOT NULL,
   `last_name` varchar(100) NOT NULL,
   `phone` varchar(100) NOT NULL,
@@ -163,14 +153,15 @@ CREATE TABLE `owner` (
 -- Dumping data for table `owner`
 --
 
-INSERT INTO `owner` (`ownerID`, `holdingNumber`, `first_name`, `last_name`, `phone`, `email`, `image`) VALUES
-(1, 12345, 'Lionel', 'Messi', '+8801813548764', 'messi10@gmail.com', ''),
-(2, 12346, 'Neymar', 'Jr', '+8801813548782', 'neymar10@gmail.com', ''),
-(3, 12347, 'Cristiano', 'Ronaldo', '+8801813548796', 'cr7@gmail.com', ''),
-(3, 12348, 'Cristiano', 'Ronaldo', '+8801813548796', 'cr7@gmail.com', ''),
-(4, 12346, 'Kylian', 'Mbappe', '+8801813548745', 'km7@gmail.com', ''),
-(9, 12332, 'Mohammad', 'Salah', '2187348', 'salah11@gmail.com', ''),
-(9, 12333, 'Mohammad', 'Salah', '2187348', 'salah11@gmail.com', '');
+INSERT INTO `owner` (`first_name`, `last_name`, `phone`, `email`, `image`) VALUES
+('Julian', 'Alvarez', '0238741', 'alvarez19@gmail.com', 'owner/alvarez19@gmail.com.jpg'),
+('Cristiano', 'Ronaldo', '+8801813548796', 'cr7@gmail.com', 'owner\\cr7@gmail.com.png'),
+('Erling', 'Haaland', '2187348', 'haaland9@gmail.com', 'owner\\haaland9@gmail.com.jpg'),
+('Kylian', 'Mbappe', '2342374', 'km7@gmail.com', 'owner\\km7@gmail.com.jpg'),
+('Emi', 'Martinez', '2341234', 'martinez23@gmail.com', 'owner/martinez23@gmail.com.jpg'),
+('Lionel', 'Messi', '+8801813548764', 'messi10@gmail.com', 'owner\\messi10@gmail.com.jpg'),
+('Neymar', 'Jr', '+8801813548782', 'neymar10@gmail.com', 'owner\\neymar10@gmail.com.jpg'),
+('Mohammad', 'Salah', '2187348', 'salah11@gmail.com', 'owner\\salah11@gmail.com.jpg');
 
 -- --------------------------------------------------------
 
@@ -179,8 +170,7 @@ INSERT INTO `owner` (`ownerID`, `holdingNumber`, `first_name`, `last_name`, `pho
 --
 
 CREATE TABLE `tenant` (
-  `holdingNumber` bigint(20) NOT NULL,
-  `apartmentid` varchar(100) NOT NULL,
+  `ApartmentID` varchar(100) NOT NULL,
   `first_name` varchar(100) NOT NULL,
   `last_name` varchar(100) NOT NULL,
   `phone` varchar(100) NOT NULL,
@@ -195,20 +185,21 @@ CREATE TABLE `tenant` (
 -- Dumping data for table `tenant`
 --
 
-INSERT INTO `tenant` (`holdingNumber`, `apartmentid`, `first_name`, `last_name`, `phone`, `email`, `image`, `nid`, `fdesc`, `registerddate`) VALUES
-(12345, '1A', 'Emam', 'Hossain', '01628615864', 'emam10@gmail.com', '', '8705214356', '', '2023-02-01'),
-(12345, '1B', 'Elon', 'Musk', '0181237641289', 'elon.musk@gmail.com', '', '2183741234', '', '2023-01-01'),
-(12345, '1D', 'Jahedul', 'Sumon', '0181384223', 'Sumon66@gmail.com', '', '7641234651', '', '2022-10-01'),
-(12346, '1A', 'Tajbir', 'Ahmed', '01811623515', 'tajbir11@gmail.com', '', '2341236493', '', '2022-11-05'),
-(12346, '1B', 'Robiul', 'Apu', '0181384752', 'Robiul34@gmail.com', '', '234124326', '', '2022-12-12'),
-(12346, '1D', 'Emam', 'Easin', '0181384298', 'Easin78@gmail.com', '', '5626234510', '', '2022-08-15'),
-(12347, '1A', 'Salauddin', 'Shible', '0123456778', 'shible10@gmail.com', '', '2341345367', '', '2023-01-11'),
-(12347, '1B', 'Yeasin', 'Arafat', '0181384234', 'Yeasin69@gmail.com', '', '5463563243', '', '2023-01-23'),
-(12347, '1C', 'Lionel', 'Messi', '+8801811623615', 'messi10@gmail.com', '', '23471283748291', 'total 7 members', '2023-02-15'),
-(12347, '1D', 'Aoual', 'Imtiaj', '0181384243', 'imtiaj6969@gmail.com', '', '895723452', '', '2023-01-01'),
-(12348, '1A', 'Md.', 'Shahariad', '01832457381', 'shahriad@hotmail.com', '', '23146751234', '', '2023-01-01'),
-(12348, '1B', 'Mehedi', 'Sabbir', '0181384267', 'Sabbir35@gmail.com', '', '7465746573', '', '2023-01-01'),
-(12348, '1D', 'Riduan', 'Islam', '0181384290', 'riduan55@gmail.com', '', '452343457', '', '2022-07-01');
+INSERT INTO `tenant` (`ApartmentID`, `first_name`, `last_name`, `phone`, `email`, `image`, `nid`, `fdesc`, `registerddate`) VALUES
+('12346-1A', 'Tajbir', 'Ahmed', '01811623515', 'tajbir11@gmail.com', '', '2341236493', '', '2022-11-05'),
+('12346-1B', 'Robiul', 'Apu', '0181384752', 'Robiul34@gmail.com', '', '234124326', '', '2022-12-12'),
+('12346-1C', 'Shajidul', 'Islam', '1811623615', 'shajidul@gmail.com', '', '23471283748291', 'Four girlfriend and no wife', '2023-02-18'),
+('12346-1D', 'Emam', 'Easin', '0181384298', 'Easin78@gmail.com', '', '5626234510', '', '2022-08-15'),
+('12346-2A', 'Sanim', 'Sourav', '32487520347', 'sourav69@gmail.com', '', '832475234', 'trying to find a girl', '2023-02-18'),
+('12347-1A', 'Salauddin', 'Shible', '0123456778', 'shible10@gmail.com', '', '2341345367', '', '2023-01-11'),
+('12347-1B', 'Yeasin', 'Arafat', '0181384234', 'Yeasin69@gmail.com', '', '5463563243', '', '2023-01-23'),
+('12347-1C', 'Lionel', 'Messi', '+8801811623615', 'messi10@gmail.com', '', '23471283748291', 'total 7 members', '2023-02-15'),
+('12347-1D', 'Aoual', 'Imtiaj', '0181384243', 'imtiaj6969@gmail.com', '', '895723452', '', '2023-01-01'),
+('12348-1A', 'Md.', 'Shahariad', '01832457381', 'shahriad@hotmail.com', '', '23146751234', '', '2023-01-01'),
+('12348-1B', 'Mehedi', 'Sabbir', '0181384267', 'Sabbir35@gmail.com', '', '7465746573', '', '2023-01-01'),
+('12348-1D', 'Riduan', 'Islam', '0181384290', 'riduan55@gmail.com', '', '452343457', '', '2022-07-01'),
+('12349-1A', 'Marcus', 'Rashford', '1811623615', 'rashford10@gmail.com', 'tenant/12349-1A.jpg', '23471283748291', 'Single', '2023-02-19'),
+('12349-1B', 'Bruno', 'Fernandez', '83745230485', 'bruno8@gmail.com', 'tenant/12349-1B.png', '2837401234', 'Married ', '2023-02-19');
 
 -- --------------------------------------------------------
 
@@ -227,9 +218,16 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`username`, `password`, `type`) VALUES
-('123471C@ras.com', '23471283748291', 'tenant'),
+('12346-1C@ras.com', '23471283748291', 'tenant'),
+('12346-2A@ras.com', '832475234', 'tenant'),
+('12347-1C@ras.com', '23471283748291', 'tenant'),
+('12349-1A@ras.com', '23471283748291', 'tenant'),
+('12349-1B@ras.com', '1234', 'tenant'),
+('alvarez19@gmail.com', '1234', 'owner'),
 ('cr7@gmail.com', 'suiii', 'owner'),
+('haaland9@gmail.com', '1234', 'owner'),
 ('km7@gmail.com', 'psgfan', 'owner'),
+('martinez23@gmail.com', '1234', 'owner'),
 ('messi10@gmail.com', '123321', 'owner'),
 ('neymar10@gmail.com', 'natalia', 'owner'),
 ('salah11@gmail.com', '1234', 'owner'),
@@ -243,7 +241,9 @@ INSERT INTO `user` (`username`, `password`, `type`) VALUES
 -- Indexes for table `apartment`
 --
 ALTER TABLE `apartment`
-  ADD PRIMARY KEY (`ApartmentID`,`holdingNumber`);
+  ADD PRIMARY KEY (`ApartmentID`),
+  ADD KEY `ApartmentID` (`ApartmentID`),
+  ADD KEY `holdingNumber` (`holdingNumber`);
 
 --
 -- Indexes for table `building`
@@ -256,25 +256,38 @@ ALTER TABLE `building`
 -- Indexes for table `location`
 --
 ALTER TABLE `location`
-  ADD PRIMARY KEY (`holdingNumber`);
+  ADD PRIMARY KEY (`holdingNumber`),
+  ADD KEY `holdingNumber` (`holdingNumber`);
+
+--
+-- Indexes for table `own`
+--
+ALTER TABLE `own`
+  ADD PRIMARY KEY (`email`,`holdingNumber`),
+  ADD KEY `email` (`email`),
+  ADD KEY `holdingNumber` (`holdingNumber`);
 
 --
 -- Indexes for table `owner`
 --
 ALTER TABLE `owner`
-  ADD PRIMARY KEY (`ownerID`,`holdingNumber`);
+  ADD PRIMARY KEY (`email`),
+  ADD KEY `email` (`email`);
 
 --
 -- Indexes for table `tenant`
 --
 ALTER TABLE `tenant`
-  ADD PRIMARY KEY (`holdingNumber`,`apartmentid`);
+  ADD PRIMARY KEY (`ApartmentID`),
+  ADD KEY `apartmentid` (`ApartmentID`),
+  ADD KEY `email` (`email`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`username`);
+  ADD PRIMARY KEY (`username`),
+  ADD KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -284,13 +297,42 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `building`
 --
 ALTER TABLE `building`
-  MODIFY `holdingNumber` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12350;
+  MODIFY `holdingNumber` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12354;
 
 --
--- AUTO_INCREMENT for table `owner`
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `apartment`
+--
+ALTER TABLE `apartment`
+  ADD CONSTRAINT `apartment_ibfk_1` FOREIGN KEY (`holdingNumber`) REFERENCES `building` (`holdingNumber`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `location`
+--
+ALTER TABLE `location`
+  ADD CONSTRAINT `location_ibfk_1` FOREIGN KEY (`holdingNumber`) REFERENCES `building` (`holdingNumber`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `own`
+--
+ALTER TABLE `own`
+  ADD CONSTRAINT `own_ibfk_1` FOREIGN KEY (`email`) REFERENCES `owner` (`email`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `own_ibfk_2` FOREIGN KEY (`holdingNumber`) REFERENCES `building` (`holdingNumber`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `owner`
 --
 ALTER TABLE `owner`
-  MODIFY `ownerID` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  ADD CONSTRAINT `owner_ibfk_1` FOREIGN KEY (`email`) REFERENCES `user` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tenant`
+--
+ALTER TABLE `tenant`
+  ADD CONSTRAINT `tenant_ibfk_1` FOREIGN KEY (`ApartmentID`) REFERENCES `apartment` (`ApartmentID`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
