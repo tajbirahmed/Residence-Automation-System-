e271356a345d3b7a27eeae8f979e3f240b1ebf9c
tajbirahmed <103434615+tajbirahmed@users.noreply.github.com>
2023-02-19 17:49:27 +0600

    19-02-23
    Version-2
    Author: Tajbir

--- home/_nav.php
+++ home/_nav.php
@@ -7,7 +7,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
     <li class="nav-item active">
-        <a class="nav-link" href="Building/Apartments/index.php">Apartment View <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="Building/Apartments/index.php">Explore Apartments<span class="sr-only">(current)</span></a>
       </li>
       <?php  
             if (!isset($_SESSION['email']) && !isset($_SESSION['type'])) {
@@ -15,9 +15,23 @@
               echo '<li class="nav-item active">
                       <a class="nav-link" href="ProfileSystem/login.php">Login <span class="sr-only">(current)</span></a>
                       </li>
-                      <li class="nav-item">
-                   <a class="nav-link" href="ProfileSystem/signup.php">Sign Up</a>
+                      <li class="nav-item active">
+                   <a class="nav-link" href="ProfileSystem/signup.php">Sign Up<span class="sr-only">(current)</span></a>
                     </li>';
+            }
+            if (isset($_SESSION['email']) && isset($_SESSION['type'])) {
+              if ($_SESSION['type'] == 'owner'){
+                  echo '<li class="nav-item active">
+                  <a class="nav-link" href="ownerInterface.php">My Profile<span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item">'; 
+                }
+              else if ($_SESSION['type'] == 'tenant') {
+                echo '<li class="nav-item active">
+                  <a class="nav-link" href="tenant/tenant_profile.php">My Profile<span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item">';
+              }
             }
             ?>
             <?php 
