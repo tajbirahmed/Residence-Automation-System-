2abc4e528b81a50f8a0c0b8747f24ebfbab0a159
Salauddin Shible <73302665+shible77@users.noreply.github.com>
2023-02-21 02:12:07 +0600

    20 feb 2023
    version-3
    Author-Salauddin

--- home/_nav_from_show_building_info.php
+++ home/_nav_from_show_building_info.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="index.php">Home</a>
+  <a class="navbar-brand" href="../index.php">Home</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -7,24 +7,36 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
     <li class="nav-item active">
-        <a class="nav-link" href="Building/Apartments/index.php">Explore Apartments<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="../Building/Apartments/index.php">Explore Apartments<span class="sr-only">(current)</span></a>
       </li>
           <?php  
             if (!isset($_SESSION['email']) && !isset($_SESSION['type'])) {
               
               echo '<li class="nav-item active">
-                  <a class="nav-link" href="ProfileSystem/login.php">Login <span class="sr-only">(current)</span></a>
+                  <a class="nav-link" href="../ProfileSystem/login.php">Login <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="ProfileSystem/signup.php">Sign Up</a>
+                  <a class="nav-link" href="../ProfileSystem/signup.php">Sign Up</a>
                         </li>';
               }
             ?>
     <?php 
         if (isset($_SESSION['email']) && isset($_SESSION['type'])){
-          echo '<li class="nav-item">
-            <a class="nav-link" href="ProfileSystem/logout.php">Logout</a>
+          echo '<li class="nav-item active">
+            <a class="nav-link" href="../ProfileSystem/logout.php">Logout</a>
           </li>';
+          if ($_SESSION['type'] == 'tenant'){ 
+              echo '<li class="nav-item active">
+              <a class="nav-link" href="../tenant/tenant_profile.php">My Profile</a>
+              </li>';
+
+          }
+          if ($_SESSION['type'] == 'owner'){ 
+            echo '<li class="nav-item active">
+            <a class="nav-link" href="../ownerInterface.php">My Buildings</a>
+            </li>';
+
+        }
         }
           ?>
       
