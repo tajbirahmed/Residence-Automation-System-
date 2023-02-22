cb1fe511277c4f42fbb973704af24b5851ffc8c3
emamhossaineasin <120816384+emamhossaineasin@users.noreply.github.com>
2023-02-23 02:03:11 +0600

    22-02-2023
    Version-04
    Author: Emam Hossain

--- home/_nav_from_aparmtent_view.php
+++ home/_nav_from_aparmtent_view.php
@@ -24,7 +24,7 @@
                   <a class="nav-link" href="../../ProfileSystem/logout.php">Logout</a>
                 </li>';
                }
-               if (isset($_SESSION['email'])) {
+               if (isset($_SESSION['email']) && $_SESSION['type'] == 'owner') {
                 echo '<li class="nav-item active">
                 <a class="nav-link" href="../../ownerInterface.php">Enlisted Building<span class="sr-only">(current)</span></a>
                 </li>
@@ -32,7 +32,10 @@
                 echo '<li class="nav-item active">
                 <a class="nav-link" href="../../owner/owner_profile.php">My Profile<span class="sr-only">(current)</span></a>
                 </li>
-                <li class="nav-item">'; 
+                <li class="nav-item">';
+                echo '<li class="nav-item active">
+                            <a class="nav-link" href="../../payment/payment_statement.php">Rent Statments</a>
+                            </li>'; 
                 
             }
       ?>
