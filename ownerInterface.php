166001e99c570ffed734f0536add8686b4ab3a7d
emamhossaineasin <120816384+emamhossaineasin@users.noreply.github.com>
2023-03-02 02:24:19 +0600

    final version
    Author: Emam Hossain

--- ownerInterface.php
+++ ownerInterface.php
@@ -10,6 +10,14 @@
     include_once('connect.php'); 
     include_once('functions.php'); 
     require_once('home/_nav_from_ownerinterface.php');
+    if (isset($_GET['send_mail']) && $_GET['send_mail'] == 1) {
+        echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>Success!</strong> Email sent successful.
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>';
+    }
     if (isset($_SESSION['type'])) {
          
         if ($_SESSION['type'] == 'owner') {
@@ -50,14 +58,20 @@
                         $image = $row1['image']; 
                         $update='update';
                         $delete='delete';
+                        $month = date('Y-m');
                         echo '<tr>
                                 <td>'.$row1['buildingName'].'</td>
                                 <td>'.$hld.'</td>
-                                <td><img src="images/'.$image.'" style="width: 150px;"></td>
+                                <td><img src="images/'.$image.'" style="width: 150px; height: 150px;"></td>
                                 <td>
                                     <a href="owner/showbuildinginfo.php?showHolding='.$hld.'"><button class="btn btn-primary">View</button></a>
-                                    <a href="Building/modify_building.php?id='.$hld.'&action='.$update.'"><button class="btn btn-success">Update</button></a>
+                                    <a href="Building/modify_building.php?id='.$hld.'&action='.$update.'"><button class="btn btn-success">Update</button></a> <br>
+                                    <br>
                                     <a href="Building/modify_building.php?id='.$hld.'&action='.$delete.'"><button class="btn btn-danger">Delete</button></a>
+                                    <a href="email_sender/send_mail.php?id='.$hld.'"><button class="btn btn-alert">Notify</button></a> <br>
+                                    <br>
+                                    <a href="owner/view_staff.php?id='.$hld.'"><button class="btn btn-primary">View Staffs</button></a> 
+                                    <a href="payment/financial_report.php?hld='.$hld.'&month='.$month.'" target="_blank"><button class="btn btn-dark">Financial Report</button></a> <br><br>
                                 </td>
                                 ';
                     }            
@@ -87,7 +101,14 @@
         <title>Building</title>
     </head>
     <body>
-        <div class="container my-5">
-            <a href="Building/addbuilding.php?id=<?php echo $email; ?>"><button>Add New Building</button>
+        <?php 
+            
+        ?>
+        <div class="container my-5" style="text-align: 20%">
+            <h1>Actions</h1>
+            <a href="Building/addbuilding.php?id=<?php echo $email; ?>"><button class="btn btn-success">Add New Building</button>
+            <a href="Building/request_authority.php"><button class="btn btn-info">Request Ownership</button>
+            <a href="Building/view_request.php"><button class="btn btn-info">Ownership Requests</button>
+            
         </div>
     </body>
