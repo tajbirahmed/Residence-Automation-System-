5aae20beafbbe92fbab3781f98e3e4ff3e05551c
Shahriad50 <82698995+Shahriad50@users.noreply.github.com>
2023-03-02 02:10:55 +0600

    Final Version
    author: Shahriad

--- Building/request_verify.php
+++ Building/request_verify.php
@@ -1,1 +1,19 @@
 
+<?php
+    session_start(); 
+    include_once('../connect.php'); 
+    if (isset($_SESSION['email']) && isset($_GET['holding'])) {
+        $email = $_SESSION['email']; 
+        $hld = $_GET['holding'];
+        $sql = "INSERT INTO `ownership_request` (`holdingNumber`, `email`, `status`) 
+                                                values('$hld', '$email', 0)"; 
+
+        $result = mysqli_query($con, $sql);
+        if ($result) {
+            // success
+            header('Location: request_authority.php');
+        } else {
+            // fail
+        }
+    }
+?>
