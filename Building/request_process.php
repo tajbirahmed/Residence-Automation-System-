d60e9ccfd3c9bab467b44e8e5b3752daff76bd28
Shahriad50 <82698995+Shahriad50@users.noreply.github.com>
2023-03-02 02:09:34 +0600

    final version
    01-03-2023
    author: Shahriad

--- Building/request_process.php
+++ Building/request_process.php
@@ -1,1 +1,22 @@
 
+<?php
+    session_start();
+    include_once('../connect.php'); 
+    if (isset($_SESSION['email'])) {
+        if (isset($_GET['email']) && isset($_GET['holding']) && isset($_GET['status'])) {
+            $email = $_GET['email']; 
+            $holding = $_GET['holding']; 
+            $status = $_GET['status'];
+            if ($status == 1) {
+                $sql = "INSERT INTO `own` (`holdingNumber`,`email`) 
+                                            values('$holding', '$email')";
+                mysqli_query($con, $sql); 
+                
+            }
+            $sql = "UPDATE `ownership_request` 
+                                        set status = 1 
+                                        where holdingNumber = '$holding' and email = '$email'" ;
+            mysqli_query($con, $sql); 
+        }
+    }
+?>
